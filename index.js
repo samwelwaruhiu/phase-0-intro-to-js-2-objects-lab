77fda295e63cb5ed9e5d630e5a64a376a860b840
Samwel Waruhiu <samweliwaruhiu@gmail.com>
2022-08-08 13:09:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+
+
 // Write your solution in this file!
+const employee = {names: "Sam", streetAdress : "00100"};
+
+function updateEmployeeWithKeyAndValue(employee,key,value ){
+    
+    return { ...employee,[key]:value};
+}
+ function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+     employee [key] = value;
+     return employee;
+
+ }
+ function deleteFromEmployeeByKey(employee,key){
+    const sem = { ...employee};
+    delete sem[key];
+    return sem;
+
+ }
+ function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
